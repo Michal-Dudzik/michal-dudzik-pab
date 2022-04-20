a892e155fbafa4bd95e7cc0749873f6d2674d96d
Michal-Dudzik <dumichal20@wp.pl>
2022-04-20 18:46:56 +0200

    basic template

---
+++ michal-dudzik-pab/projekt2_ksiazka/gulpfile.js
@@ -0,0 +1,7 @@
+var gulp = require("gulp");
+var typescript = require("gulp-typescript");
+var small = require("small").gulp;
+var sourcemaps = require("gulp-sourcemaps");
+var merge = require("merge2");
+var concat = require("gulp-concat");
+var uglify = require("gulp-uglify");
