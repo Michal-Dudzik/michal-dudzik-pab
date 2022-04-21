c6cd47689a7fbe464988df6da3e832be61bfb9e7
Michal-Dudzik <dumichal20@wp.pl>
2022-04-21 12:21:52 +0200

    just synchro

--- michal-dudzik-pab/projekt2_ksiazka/gulpfile.js
+++
@@ -1,7 +0,0 @@
-var gulp = require("gulp");
-var typescript = require("gulp-typescript");
-var small = require("small").gulp;
-var sourcemaps = require("gulp-sourcemaps");
-var merge = require("merge2");
-var concat = require("gulp-concat");
-var uglify = require("gulp-uglify");
